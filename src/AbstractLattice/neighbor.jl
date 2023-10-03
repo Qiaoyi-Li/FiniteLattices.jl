f9a07428dfe9fbafea428acfc118d2d14005d2cb
qyli <qiaoyili@buaa.edu.cn>
2023-10-03 15:54:13 +0800

    support ordered pairs for neighbor

--- src/AbstractLattice/neighbor.jl
+++ src/AbstractLattice/neighbor.jl
@@ -12,6 +12,9 @@ Determine the distance between the 2 sites of a pair.
 
      d::Real = metric(Latt, r)
 Given a distance directly. Note `d` will be used if both `r` and `d` are given.
+
+     ordered::Bool = false
+View `(i,j)` as an ordered pair if `true`, i.e. `(i,j) ≠ (j,i)`. 
 """
 function neighbor(Latt::AbstractLattice, i::Int64; kwargs...)
      d = _get_d(Latt; kwargs...)
@@ -21,7 +24,9 @@ function neighbor(Latt::AbstractLattice; kwargs...)
      d = _get_d(Latt; kwargs...)
      Pairs = NTuple{2, Int64}[]
      for i = 1:length(Latt), j = i+1:length(Latt)
-          isapprox(metric(Latt, i, j), d) && push!(Pairs, (i,j)) 
+          !isapprox(metric(Latt, i, j), d) && continue
+          push!(Pairs, (i,j)) 
+          get(kwargs, :ordered, false) && push!(Pairs, (j,i))
      end
      return Pairs
 end
