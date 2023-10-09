2b4672bd6af68db70b29e6293a044371fb78b4f6
qyli <qiaoyili@buaa.edu.cn>
2023-10-09 14:57:58 +0800

    resolve iterate

---
+++ src/SquaLatt/iterate.jl
@@ -0,0 +1,55 @@
+function Base.iterate(::Type{<:SquareLattice})
+     return (1,1), ((1,1), 1)
+end
+
+Base.length(::Type{<:SquareLattice{L, W}}) where {L, W} = L*W
+
+function Base.iterate(::Type{<:Union{OpenSquare{L, W, ZigzagPath}, Cylinder{L, W, ZigzagPath}}},
+     state::Tuple{NTuple{2, Int64}, Int64}) where {L, W}
+     ((x, y), i) = state
+     i == L * W && return nothing
+
+     site = y < W ? (x, y+1) : (x+1, 1)
+
+     return site, (site, i+1)
+end
+
+function Base.iterate(::Type{<:Union{OpenSquare{L, W, SnakePath}, Cylinder{L, W, SnakePath}}},
+     state::Tuple{NTuple{2, Int64}, Int64}) where {L, W}
+     ((x, y), i) = state
+     i == L * W && return nothing
+
+     if isodd(x)
+          site = y < W ? (x, y+1) : (x+1, y)
+     else
+          site = y > 1 ? (x, y-1) : (x+1, y)
+     end
+
+     return site, (site, i+1)
+end
+
+function Base.iterate(::Type{<:Union{OpenSquare{L, W, DiagonalPath}, Cylinder{L, W, DiagonalPath}}},
+     state::Tuple{NTuple{2, Int64}, Int64}) where {L, W}
+     ((x, y), i) = state
+     i == L * W && return nothing
+
+     if iseven(x + y)
+          if y == W
+               site = (x + 1, y)
+          elseif x == 1
+               site = (x, y + 1) 
+          else
+               site = (x - 1, y + 1)
+          end
+     else
+          if x == L
+               site = (x, y + 1)
+          elseif y == 1
+               site = (x + 1, y)
+          else
+               site = (x + 1, y - 1)
+          end
+     end
+
+     return site, (site, i+1)
+end
